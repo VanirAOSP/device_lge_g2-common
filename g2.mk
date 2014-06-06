38bad543071ee4925b4010bf79383e305348a715
bart452 <bart_96@live.nl>
2014-06-06 20:37:16 +0200

    g2: cleanup and quiet logspam
    Conflicts:
    	g2.mk

--- g2.mk
+++ g2.mk
@@ -230,19 +230,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=480
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=72 \
-    ro.hwui.layer_cache_size=48 \
-    ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.path_cache_size=32 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=1024
-
-PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true \
     ro.loki_enabled=1 \
 
@@ -278,8 +265,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Enable some debug messages by default
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.qualcomm.sns.hal=w \
-    debug.qualcomm.sns.daemon=w \
+    debug.qualcomm.sns.hal=i \
+    debug.qualcomm.sns.daemon=i \
     debug.qualcomm.sns.libsensor1=w
 
 # Ril 
@@ -335,7 +322,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     af.resampler.quality=255 \
     audio.offload.buffer.size.kb=32 \
     audio.offload.gapless.enabled=false \
-    av.offload.enable=true \
+    av.offload.enable=true
 
 # for powerhal
 PRODUCT_PROPERTY_OVERRIDES += \
