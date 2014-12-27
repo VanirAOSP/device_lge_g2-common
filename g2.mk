9b9bdfc6fb7aaf8b58a73774ac9d3c089eb1f99a
DHOMD <DHO@dho.im>
2014-12-27 12:52:31 -0500

    Remove DSPM residuals
    Change-Id: I62a88875fc356f61eb153bb6dcb97df0c6532b47

--- g2.mk
+++ g2.mk
@@ -33,7 +33,6 @@ PRODUCT_COPY_FILES += \
 
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
