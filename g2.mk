484a4ac6ae7348df1bdc7c958ed192d2dc930e43
sudosurootdev <sudosurootdev@gmail.com>
2015-01-25 21:40:04 -0500

    Add sap.conf for gyro
    Change-Id: I52b1c5afc79b14b0c3429a1b61bd0c4a6cfceebf

--- g2.mk
+++ g2.mk
@@ -42,7 +42,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/configs/sap.conf:system/etc/sap.conf
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermald.conf:system/etc/thermald.conf \
